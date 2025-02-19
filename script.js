b33bbc5378527b478ed14620b55f2f36b90a8568
Sintayehu <sentflix@gmail.com>
2025-02-20 02:11:45 +0300

    refactore the code

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 
 
 const sun = document.querySelector('.sun');
-      const dayNight = document.querySelector('.dayNight');
       // const img = document.querySelector('.img');
       sun.addEventListener('click', () => {
-        dayNight.classList.toggle('night');
+       body.classList.toggle('night');
       });
