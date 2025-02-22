7a5e86c1b2ad3a57805675e4bf6a19928535dc7f
Sintayehu <sentflix@gmail.com>
2025-02-22 12:35:55 +0300

    assign variables

--- script.js
+++ script.js
@@ -2,4 +2,11 @@
 
 const img = document.querySelector('.img')
 const sun = document.querySelector('.sun');
-const 
+const todoSearch = document.querySelector('.todoSearch');
+const addTask = document.querySelector('.add-task');
+const todoInput = document.querySelector('.TodoInput');
+const todoList = document.querySelector('.todo-list');
+const followupButtons = document.querySelector('.followupButtons');
+const itemLeft = document.querySelector('.items-left');
+const filter = document.querySelector('.filter');
+const clearCompleted = document.querySelector('.clear-completed');
