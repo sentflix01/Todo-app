478ef2a600f251412267d153b4d11df75a45ea3d
Sintayehu <sentflix@gmail.com>
2025-03-04 10:31:50 +0300

    create element

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
-
-
 // const img = document.querySelector('img')
 // const sun = document.querySelector('sun');
-const todoSearch = document.querySelector('todoSearch');
-const addTask = document.getElementById('add-task');
-const todoInput = document.getElementById('todo-input');
-const todoList = document.querySelector('todo-list');
+const todoSearch = document.querySelector(".todoSearch");
+// const addTask = document.getElementById('add-task');
+const todoInput = document.getElementById("todo-input");
+// const todoForm = document.querySelector('form');
+const todoList = document.querySelector(".todo-list"); // Fixed selector
 // const followupButtons = document.querySelector('followupButtons');
 // const itemLeft = document.querySelector('items-left');
 // const filter = document.querySelector('.filter');
@@ -13,12 +12,38 @@ const todoList = document.querySelector('todo-list');
 
 let allTodos = [];
 
-addTask.addEventListener("submit", (e) => {
-    e.preventDefault();
-    addTodo();
-})
+todoSearch.addEventListener("submit", (e) => {
+  e.preventDefault();
+  addTodo();
+});
 
 function addTodo() {
   const todoText = todoInput.value.trim();
-  alert(todoText);
+  if (todoText.length > 0) {
+    // Fixed condition
+    allTodos.push(todoText);
+    updateTodoList(); // Call updateTodoList to refresh the list
+    todoInput.value = ""; // Clear input after adding
+  }
+}
+
+function createTodoItem(todo, index) {
+  const todoLi = document.createElement("LIst"); // Fixed element type
+  const todoId = "todo-" + index;
+  todoLi.className = "list";
+
+  todoLi.innerHTML = `
+      <input type="checkbox" id="${todoId}" />
+   <label for="${todoId}" class="list-item-checkbox"></label>
+   <label for="${todoId}" class="list-item-text">${todo} </label>
+  <div class="list-item-delete-icon">❌</div>`;
+  return todoLi;
+}
+
+function updateTodoList() {
+  todoList.innerHTML = "";
+  allTodos.forEach((todo, index) => {
+    const todoItem = createTodoItem(todo, index);
+    todoList.appendChild(todoItem);
+  });
 }
